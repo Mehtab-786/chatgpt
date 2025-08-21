cf98c42996bea9925bb6524b0efc9c8c6c3a34cf
Mehtab Hussain <tradercoder786@gmail.com>
2025-08-21 16:58:12 +0530

    refactor: remove obsolete files and clean up project structure

--- src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { authUser } = require('../middlewares/authUser.middlewares')
-const { createChat } = require('../controllers/chat.controllers')
-const chatRouter = express.Router()
-
-chatRouter.post('/',authUser, createChat)
-
-module.exports = chatRouter
